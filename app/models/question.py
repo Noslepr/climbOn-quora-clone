c53dfde9c902260e1122a42b5da2523cb9649d43
Chris Young <ChrisC.Young1@gmail.com>
2022-03-14 16:57:04 -0700

    user, question, and answer models created

---
+++ app/models/question.py
@@ -0,0 +1,19 @@
+from .db import db
+
+class Question(db.Model):
+    __tablename__ = 'questions'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    question = db.Column(db.String(1000), nullable=False)
+
+    user = db.relationship('User', back_populates='questions')
+    answers = db.relationship('Answer', back_populates='question')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user.to_dict(),
+            'question': self.question,
+            'answers': [answer.to_dict for answer in self.answers]
+        }
