5129ba9a1cc11a85b212899f522bee7ee9342114
Chris Young <ChrisC.Young1@gmail.com>
2022-03-15 14:12:43 -0700

    All question routes are working with form validation

--- app/forms/question_form.py
+++ app/forms/question_form.py
@@ -11,8 +11,13 @@ def question_starts_with(form, field):
         if question.startswith(prefix):
             return
 
-    raise ValidationError('Question needs to start with: What, Who, When, Where, Why, or How')
+    raise ValidationError('Question needs to start with: What, Who, When, Where, Why, or How.')
 
+def question_ends_with(form, field):
+    question = field.data
+
+    if not question.endswith('?'):
+        raise ValidationError('Please make sure your question ends with ?.')
 
 class QuestionForm(FlaskForm):
-    question = StringField('question', validators=[DataRequired(), question_starts_with])
+    question = StringField('question', validators=[DataRequired(), question_starts_with, question_ends_with])
