0f7f3b466742145b90958d15b17b2f6caffe29f4
Chris Young <ChrisC.Young1@gmail.com>
2022-03-15 19:30:20 -0700

    login/splash styled

---
+++ react-app/src/components/navbar/NavBar.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+// import { useSelector } from 'react-redux';
+import LogoutButton from '../auth/LogoutButton';
+
+const NavBar = ({ user }) => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <LogoutButton />
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
