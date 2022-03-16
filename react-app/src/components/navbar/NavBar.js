8521c6cf090e4a6a0a3d05cf54476887225b488b
Chris Young <ChrisC.Young1@gmail.com>
2022-03-16 11:26:26 -0700

    component for post question working

--- react-app/src/components/navbar/NavBar.js
+++ react-app/src/components/navbar/NavBar.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LogoutButton from '../auth/LogoutButton';
+import { PostQuestion } from '../postQuestion/PostQuestion'
+import { Modal } from '../../context/Modal'
 import './NavBar.css'
 
 export const NavBar = ({user}) => {
+    const [showQuestionModal, setShowQuestionModal] = useState(false)
+
+
+    const askQuestion = e => {
+        e.preventDefault()
+        setShowQuestionModal(true)
+    }
 
     return (
         <nav id='nav-bar'>
 
             <LogoutButton />
+            <button onClick={askQuestion}>Add question</button>
+
+            {showQuestionModal &&
+                <Modal onClose={() => setShowQuestionModal(false)}>
+                    <PostQuestion setShowQuestionModal={setShowQuestionModal}/>
+                </Modal>
+            }
         </nav>
     )
 }
--- react-app/src/components/postQuestion/PostQuestion.js
+++ react-app/src/components/postQuestion/PostQuestion.js
@@ -1,18 +1,25 @@
 import React, { useEffect, useState } from 'react';
 
 
-export const PostQuestion = ({}) => {
+export const PostQuestion = ({setShowQuestionModal}) => {
     const [question, setQuestion] = useState('')
+
+    const handleQuestion = (e) => {
+        e.preventDefault()
+        setShowQuestionModal(false)
+        console.log('question:', question)
+    }
 
     return (
         <>
-            <form>
+            <form onSubmit={handleQuestion}>
                 <input
                     type='text'
                     placeholder='question'
                     value={question}
-                    onChange={(e) => setQuestion(e.target)}
+                    onChange={(e) => setQuestion(e.target.value)}
                 />
+                <button>Add question</button>
             </form>
         </>
     )
