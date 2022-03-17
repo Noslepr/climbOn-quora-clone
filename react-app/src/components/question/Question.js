e211c0e68b60ccc49f951b9da9944304a9226f7f
Chris Young <ChrisC.Young1@gmail.com>
2022-03-17 11:47:33 -0700

    form being built

--- react-app/src/components/question/Question.js
+++ react-app/src/components/question/Question.js
@@ -1,17 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import './Question.css'
 
-
-export const Question = () => {
+export const Question = ({ user }) => {
     const { id : questionId } = useParams()
+    const [showAnswerBox, setShowAnswerBox] = useState(false)
     const question = useSelector(({questions}) => questions[questionId])
+
+    const openAnswerBox = () => {
+        setShowAnswerBox(true)
+        const answerElement = document.querySelector('#post-answer')
+        answerElement.style.opacity = '.5'
+        answerElement.style.pointerEvents = 'none'
+    }
 
     return (
         <div id='question-page'>
             <div id='question-left-container'>
-                <h1 id="question-header">{question.question}</h1>
+                <div id="question-header-container">
+                    <div id="question-header">{question.question}</div>
+                    <div id="question-footer-icons">
+                        <div id="post-answer" onClick={openAnswerBox}>
+                            <i class="fa-solid fa-pen-to-square square"></i>
+                            <div className="answers-text" id='post-answer-text'>Answer</div>
+                        </div>
+                    </div>
+                {showAnswerBox && (
+                    <div id="post-answer-box-container">
+                        <div id="post-answer-box-header">
+                            <img src='defaultUser.jpg'></img>
+                            <div>
+                                <div>{user.full_name}</div>
+                                <div>Edit credential</div>
+                            </div>
+                        </div>
+
+                    </div>
+                )}
+                </div>
                 <div id='num-answers'>
                     <div className="circle">
                         <i className="fa-solid fa-list-ul" id="answer-icon"></i>
