3f0ae56bbc952c992b0e923297072fc3b13fc0c0
Chris Young <ChrisC.Young1@gmail.com>
2022-03-21 14:53:34 -0700

    backend for cred patch

--- app/forms/question_form.py
+++ app/forms/question_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired,  ValidationError, Length
+from wtforms.validators import DataRequired,  ValidationError
 from app.models import Question
 
 def question_starts_with(form, field):
---
+++ app/forms/user_form.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import ValidationError
+from app.models import User
+
+def credential_length(form, field):
+    cred = field.data
+
+    if len(cred) < 5:
+        raise ValidationError('Credentials are too short. Please provide more detail')
+
+    if len(cred) > 30:
+        raise ValidationError('Credentials are too long. Please keep them short and specific')
+
+class UserForm(FlaskForm):
+    user = StringField('credentials', validators=[credential_length])
