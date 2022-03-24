e9fff2a95fd0367848e34dc1e6cd55124dac97da
Chris Young <ChrisC.Young1@gmail.com>
2022-03-24 13:32:31 -0700

    seeds

--- app/seeds/questions.py
+++ app/seeds/questions.py
@@ -14,10 +14,20 @@ def seed_questions():
         Question(user_id = 7, question = 'Where are the best places to climb outdoors?'),
         Question(user_id = 9, question = 'How does the grading system work in the US?'),
         Question(user_id = 1, question = 'What is the point of brushing holds?'),
+        Question(user_id = 3, question = 'How do I begin trad climbing?'),
+        Question(user_id = 5, question = 'What is the difference in equipment for ice climbing and trad climbing?'),
+        Question(user_id = 1, question = 'What is trad climbing considered so expensive?'),
+        Question(user_id = 1, question = 'What is a "trad dad"?'),
+        Question(user_id = 7, question = 'How do I find the best climbing shoes?'),
+        Question(user_id = 3, question = 'Where are the best Bay Area outdoor climbs for a beginner?'),
+        Question(user_id = 9, question = 'Who are the best professional rock climbers?'),
+        Question(user_id = 1, question = 'What is the best season to do outdoor climbing?'),
+        Question(user_id = 1, question = 'What time of day is usually best to go climbing outdoors?'),
+
+
 
         # Intermediate questions
-        Question(
-            user_id = 2, question = 'What should I do for training outside of just climbing?'),
+        Question(user_id = 2, question = 'What should I do for training outside of just climbing?'),
 
     ]
 
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,15 +4,56 @@ from app.models import db, User
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     users = [
-        User(full_name='Hrodebert Elder', email='Hrodeber@Elder.com', password='password!', credentials='Beginner climber 6mo experience'),
-        User(full_name='Ortwin Amatore', email='ortwin@amatore.com', password='password!', credentials='Intermediate climber 3yrs experience'),
-        User(full_name='Anil Warrick', email='Anil@Warrick.com', password='password!', credentials='New climber'),
-        User(full_name='Watson Soriano', email='watson@soriano.com', password='password!', credentials='Professional climber 15yrs Bouldering specialist'),
-        User(full_name='Nadab Severins', email='Nadab@Severins.com', password='password!', credentials='v0 gumby'),
-        User(full_name='Neilos Thornton', email='Neilos@Thornton.com', password='password!', credentials='Intermediate climber V6 boulderer'),
-        User(full_name='Severina Georgiev', email='Severina@Georgiev.com', password='password!', credentials='Beginner climber'),
-        User(full_name='Pipin Oliversen', email='Pipin@Oliversen.com', password='password!', credentials='5.12a redpoint outdoor'),
-        User(full_name='Flaka Meeuwes', email='Flaka@Meeuwes.com', password='password!', credentials='Just starting out'),
+        User(
+            full_name='Hrodebert Elder',
+            email='Hrodeber@Elder.com',
+            password='password!',
+            credentials='Beginner climber 6mo experience',
+            profile_img='https://climb-on.s3.amazonaws.com/4cb5b16aeff344299bfe52be624143dc.jpg'),
+        User(
+            full_name='Ortwin Amatore',
+            email='ortwin@amatore.com',
+            password='password!',
+            credentials='Intermediate climber 3yrs experience'),
+        User(
+            full_name='Anil Warrick',
+            email='Anil@Warrick.com',
+            password='password!',
+            credentials='New climber',
+            profile_img='https://climb-on.s3.amazonaws.com/4cb5b16aeff344299bfe52be624143dc.jpg'),
+        User(
+            full_name='Watson Soriano',
+            email='watson@soriano.com',
+            password='password!',
+            credentials='Professional climber 15yrs Bouldering specialist'),
+        User(
+            full_name='Nadab Severins',
+            email='Nadab@Severins.com',
+            password='password!',
+            credentials='v0 gumby',
+            profile_img='https://climb-on.s3.amazonaws.com/d3fc26ae94414a4e901bec3f7b229696.png'),
+        User(
+            full_name='Neilos Thornton',
+            email='Neilos@Thornton.com',
+            password='password!',
+            credentials='Intermediate climber V6 boulderer'),
+        User(
+            full_name='Severina Georgiev',
+            email='Severina@Georgiev.com',
+            password='password!',
+            credentials='Beginner climber',
+            profile_img='https://climb-on.s3.amazonaws.com/61b89a74cdb54a21af91ed17e1844a19.jpg'),
+        User(
+            full_name='Pipin Oliversen',
+            email='Pipin@Oliversen.com',
+            password='password!',
+            credentials='5.12a redpoint outdoor'),
+        User(
+            full_name='Flaka Meeuwes',
+            email='Flaka@Meeuwes.com',
+            password='password!',
+            credentials='Just starting out',
+            profile_img='https://climb-on.s3.amazonaws.com/cf83d1ade26341a5844dbd0150d12f7f.jpeg'),
     ]
 
     for user in users:
